006920b008c26d44a57ae2910599ec8d107157b6
Ali Ahmad <69896600+mian-ali@users.noreply.github.com>
2021-02-01 18:47:25 +0500

    V.01

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@material-ui/core";
+import ProductsGrid from "../../components/ProductsGrid/ProductsGrid.controller";
+
+const Home = () => {
+  return (
+    <Box>
+      <ProductsGrid />
+    </Box>
+  );
+};
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+
+import App from "./App";
+import theme from "./theme";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+//import store from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      {/* <Provider store={store}> */}
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </Provider> */}
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
